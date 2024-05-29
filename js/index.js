85bc3221087c989e9b47509f51ed9400cedf4a2f
unknown <joeunsu00@naver.com>
2024-05-29 18:19:12 +0900

    삼청각 클론코딩 메인페이지

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.main_slider').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+}); 
